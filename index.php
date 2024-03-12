0cea498ef26c43a66207be24871e70211c64b1fb
Aakash_dubey999 <84366886+Akwarrior222@users.noreply.github.com>
2024-03-13 01:46:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Image Display</title>
+    <title>My PHP Page</title>
 </head>
 <body>
-    <h1>Displaying an Image</h1>
-    <div>
-        <img src="C:\Users\Aakash Dubey\Downloads\lamborghini-car-lamborghini-aventador-vehicle-wallpaper-preview.jpg" alt="Image Description">
-    </div>
+    <h1>Welcome to my PHP Page</h1>
+    <p>
+        <?php
+            // PHP code can go here
+            echo "Hello, world!";
+        ?>
+    </p>
 </body>
 </html>
