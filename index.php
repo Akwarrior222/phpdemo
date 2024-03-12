f6e02348bf47aef1a53d8e02d57db6c6ddd89f23
Aakash_dubey999 <84366886+Akwarrior222@users.noreply.github.com>
2024-03-13 00:59:32 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Image Display</title>
+</head>
+<body>
+    <h1>Displaying an Image</h1>
+    <div>
+        <img src="C:\Users\Aakash Dubey\Downloads\lamborghini-car-lamborghini-aventador-vehicle-wallpaper-preview.jpg" alt="Image Description">
+    </div>
+</body>
+</html>
